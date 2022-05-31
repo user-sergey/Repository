0d890a8149e75d376cb6dfcbc5af77ebc8949d4c
G3ntleM4n <good-ok123654@mail.ru>
2022-05-31 21:28:02 +0300

    добавил типа стиль

--- WebApplication1/Controllers/CategoryController.cs
+++ WebApplication1/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web.Data;
+using Web.Models;
 
 namespace Web.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
         }
     }
 }

06d0910e6fee008519ece1764525b3a9d375522a
user-sergey <103661453+user-sergey@users.noreply.github.com>
2022-06-15 00:36:50 +0300

    Add "CreateCommentaryRequest.cs"

---
+++ WebApplication1/Models/CreateCommentaryRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.Models
+{
+    public class CreateCommentaryRequest
+    {
+        public string Content { get; set; }
+        public int userId { get; set; }
+        public int newsId { get; set; }
+    }
+}

b1f1f41ec5056cebedbce456569b58d2dd37263c
user-sergey <103661453+user-sergey@users.noreply.github.com>
2022-06-15 00:35:22 +0300

    Add "ICommentaryManager.cs" and "CommentaryManager.cs"

---
+++ Web.Logic/Commentaries/ICommentaryManager.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.Logic.Commentaries
+{
+    public interface ICommentaryManager
+    {
+        Task<IList<Commentary>> GetAll();
+        Task Create(string content, int? userid, int? newsid);
+        Task Delete(int id);
+    }
+}

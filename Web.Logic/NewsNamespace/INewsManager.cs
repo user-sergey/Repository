a672f643e24b561e73732c93c21f0b34c5238906
user-sergey <103661453+user-sergey@users.noreply.github.com>
2022-06-14 03:15:31 +0300

    Add "INewsManager.cs" and "NewsManager.cs"

---
+++ Web.Logic/NewsNamespace/INewsManager.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.Logic.NewsNamespace
+{
+    public interface INewsManager
+    {
+        Task<IList<News>> GetAll();
+        Task Create(string? name, int? categoryid, int? countryid, int? publisherid, int? sourceid);
+        Task Delete(int id);
+    }
+}

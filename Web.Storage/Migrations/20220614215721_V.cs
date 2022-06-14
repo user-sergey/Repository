84d5eab3acbe6a8e574dfbd2fa9a66c380bda45a
user-sergey <103661453+user-sergey@users.noreply.github.com>
2022-06-15 02:41:45 +0300

    Add migrations

---
+++ Web.Storage/Migrations/20220614215721_V.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.Storage.Migrations
+{
+    public partial class V : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "NewsNamespace",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "NewsNamespace");
+        }
+    }
+}
---
+++ Web.Storage/Migrations/20220614225941_VI.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.Storage.Migrations
+{
+    public partial class VI : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NewsNamespace_NewsNamespace_countryId",
+                table: "NewsNamespace");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PublisherCountries_NewsNamespace_countryId",
+                table: "PublisherCountries");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_NewsNamespace_countryId",
+                table: "Users");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NewsNamespace_Countries_countryId",
+                table: "NewsNamespace",
+                column: "countryId",
+                principalTable: "Countries",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PublisherCountries_Countries_countryId",
+                table: "PublisherCountries",
+                column: "countryId",
+                principalTable: "Countries",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Countries_countryId",
+                table: "Users",
+                column: "countryId",
+                principalTable: "Countries",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NewsNamespace_Countries_countryId",
+                table: "NewsNamespace");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PublisherCountries_Countries_countryId",
+                table: "PublisherCountries");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Countries_countryId",
+                table: "Users");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NewsNamespace_NewsNamespace_countryId",
+                table: "NewsNamespace",
+                column: "countryId",
+                principalTable: "NewsNamespace",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PublisherCountries_NewsNamespace_countryId",
+                table: "PublisherCountries",
+                column: "countryId",
+                principalTable: "NewsNamespace",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_NewsNamespace_countryId",
+                table: "Users",
+                column: "countryId",
+                principalTable: "NewsNamespace",
+                principalColumn: "Id");
+        }
+    }
+}
